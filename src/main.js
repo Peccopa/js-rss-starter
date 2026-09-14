6f7a59b5ee22e2fe92fabb8eed03679933e9491b
Andrey Zharkikh <orbital.logistics@yandex.ru>
2026-09-14 15:12:28 +0300

    feat: add component kit to javascript starter

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log('Starter Pack JS');
+import { ContainerComponent, TextComponent } from './shared/components';
+
+const title = new TextComponent({
+  tag: 'h1',
+  content: 'Hello from Component Kit!',
+  classes: 'app-title',
+});
+
+const app = new ContainerComponent({
+  id: 'app',
+  classes: 'app',
+  children: [title],
+});
+
+document.body.append(app.element);
+
+console.log('JS RSS Starter');
