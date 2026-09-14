6a8484038c1f96b80c734519bacbf10d7662b3a6
Andrey Zharkikh <orbital.logistics@yandex.ru>
2026-09-14 18:16:38 +0300

    feat: add state kit demo

--- src/main.js
+++ src/main.js
@@ -1,17 +1,56 @@
-import { ContainerComponent, TextComponent } from './shared/components';
+import {
+  ContainerComponent,
+  ButtonComponent,
+  TextComponent,
+} from './shared/component-kit';
+import StateKit from './shared/state-kit';
 
-const title = new TextComponent({
-  tag: 'h1',
-  content: 'Hello from Component Kit!',
-  classes: 'app-title',
+// === State Kit ===
+
+const initialState = { count: 0 };
+
+const stateKit = new StateKit(initialState);
+
+const increment = (state, action) => {
+  if (action.type === 'INCREMENT') {
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  }
+
+  return state;
+};
+
+stateKit.addReducer(increment);
+
+// === Component Kit ===
+
+const counter = new TextComponent({
+  tag: 'p',
+  content: `Count: ${stateKit.getState().count}`,
+});
+
+const button = new ButtonComponent({
+  content: 'Increment',
+});
+
+button.setListeners({
+  click: () => {
+    console.log(1);
+
+    stateKit.dispatch({ type: 'INCREMENT' });
+  },
+});
+
+stateKit.subscribe((state) => {
+  counter.setContent(`Count: ${state.count}`);
 });
 
 const app = new ContainerComponent({
   id: 'app',
   classes: 'app',
-  children: [title],
+  children: [counter, button],
 });
 
 document.body.append(app.element);
-
-console.log('JS RSS Starter');
